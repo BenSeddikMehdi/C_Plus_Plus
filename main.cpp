999341e16bbad8699577c77a9533360878671e5a
BenSeddikMehdi <amigoinpt@gmail.com>
2019-12-13 19:22:46 +0100

    Manipulating Pointers in C++

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main() {
     char* buffer = new char[8];
-    memset(buffer, 12, 8);
+    memset(buffer, 0x0F, 8);
     char** ptr = &buffer;
     char*** ptr_ptr = &ptr;
     return 0;
