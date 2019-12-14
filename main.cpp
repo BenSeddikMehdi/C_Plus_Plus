4aa307a6105725d1c4952d5335240959b6a90686
BenSeddikMehdi <amigoinpt@gmail.com>
2019-12-14 03:02:41 +0100

    Manipulating References in C++

--- main.cpp
+++ main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
 #include "logFunction.h"
 
+typedef struct Player {
+    int x, y;
+    int speed;
+    void moveXaYa(int xa, int ya){
+        x = xa*speed;
+        y = ya*speed;
+    }
+}player_t;
+
 int main() {
+    player_t var{};
+    var.speed = 3;
+    var.moveXaYa(3, 2);
+    print(var.x);
+    print(var.y);
+
+
 
     return 0;
 }
